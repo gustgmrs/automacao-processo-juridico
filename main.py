509f5a223ce8f92b86e6155a5ea683d2bcbc1532
Gustavo Guimarães <gusguims@outlook.com>
2023-05-10 20:27:31 -0300

    automatizando processo para utilizando uma base de dados

--- main.py
+++ main.py
@@ -3,54 +3,77 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.alert import Alert
-
-
 from time import sleep
+
+import pandas as pd
 
 # Inicializando o serviço do Chrome
 service = Service('/home/gustgmrs/Downloads/chromedriver')
 driver = webdriver.Chrome(service=service)
 
-# Abrindo o navegador
-driver.get(
-    'file:///home/gustgmrs/projetos/automacao-processo-juridico/files/index.html')
+# Criando o DataFrame
+df_processos = pd.read_excel('./files/processos.xlsx')
 
-# Escolhendo o estado
-dropdown = driver.find_element(By.XPATH, '/html/body/div/div')
-item = driver.find_element(By.XPATH, '/html/body/div/div/div/a[2]')
+# Para cada linha do DataFrame, faça:
+for i, linha in enumerate(df_processos.index):
 
-# colocar o mouse em cima do dropdown
-ActionChains(driver).move_to_element(dropdown).perform()
+    # Abrindo o navegador
+    driver.get(
+        'file:///home/gustgmrs/projetos/automacao-processo-juridico/files/index.html')
 
-# clicar no item
-item.click()
-sleep(2)
+    # colocar o mouse em cima do dropdown
+    dropdown = driver.find_element(By.XPATH, '/html/body/div/div')
+    ActionChains(driver).move_to_element(dropdown).perform()
 
-aba_principal = driver.window_handles[0]  # pega a aba principal
-aba_secundaria = driver.window_handles[1]  # pega a aba secundária
+    # Escolhendo o estado
+    cidade = df_processos.loc[linha, 'Cidade']
+    driver.find_element(By.PARTIAL_LINK_TEXT, cidade).click()
 
-# mudar para a aba secundária
-driver.switch_to.window(aba_secundaria)
-sleep(2)
+    # mudar para a aba nova
+    aba_principal = driver.window_handles[0]  # pega a aba principal
+    indice = 1 + linha  # pega o índice da aba secundária
+    nova_aba = driver.window_handles[indice]  # pega a aba secundária
+    driver.switch_to.window(nova_aba)  # muda para a aba secundária
 
-# Enviando nome do cliente
-nome = driver.find_element(By.ID, 'nome')
-nome.send_keys('Gustavo')
+    # preencher formulário
+    # Enviando nome do cliente
+    nome = driver.find_element(By.ID, 'nome')
+    nome.send_keys(df_processos.loc[linha, 'Nome'])
 
-# Enviando nome do advogado
-advogado = driver.find_element(By.ID, 'advogado')
-advogado.send_keys('Victória')
+    # Enviando nome do advogado
+    advogado = driver.find_element(By.ID, 'advogado')
+    advogado.send_keys(df_processos.loc[linha, 'Advogado'])
 
-# Enviando número do processo
-processo = driver.find_element(By.ID, 'numero')
-processo.send_keys('123456789')
+    # Enviando número do processo
+    processo = driver.find_element(By.ID, 'numero')
+    processo.send_keys(df_processos.loc[linha, 'Processo'])
 
-# Clicando no botão de pesquisar
-pesquisar = driver.find_element(By.CLASS_NAME, 'registerbtn')
-pesquisar.click()
-sleep(2)
+    # Clicando no botão de pesquisar
+    pesquisar = driver.find_element(By.CLASS_NAME, 'registerbtn')
+    pesquisar.click()
+    sleep(2)
 
-# Aceitando o alert
-alerta = Alert(driver)
-alerta.accept()
-sleep(10)
+    # Aceitando o alert
+    alerta = driver.switch_to.alert
+    alerta.accept()
+
+    # esperar o resultado da pesquisa e agir de acordo com o resultado
+    while True:
+        try:
+            alerta = driver.switch_to.alert
+            break
+        except:
+            sleep(1)
+
+    sleep(2)
+    texto_alerta = alerta.text
+
+    if 'Processo encontrado com sucesso' in texto_alerta:
+        alerta.accept()
+        df_processos.loc[linha, 'Status'] = 'Encontrado'
+    else:
+        df_processos.loc[linha, 'Status'] = 'Não encontrado'
+        alerta.accept()
+
+driver.quit()
+print(df_processos)
